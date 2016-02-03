3266d73a452ec895697527642f77f3a702c9469d
syedshabihhasan <hasanshabih@gmail.com>
2016-02-03 14:27:27 -0600

    added an option for selection factor

--- src/clustering/identifyTravel.py
+++ src/clustering/identifyTravel.py
@@ -18,10 +18,10 @@ def getalldistancesandspeeds(gps_coords, sampling_rate=0.1):
     return distances, speeds
 
 
-def istravelling(speeds, gps_coords, speed_limit=5):
+def istravelling(speeds, gps_coords, speed_limit=5, selection_factor = 0.5):
     if -1 == speeds[0]:
         return [False, 0]
     is_above_limit = []
     for idx in range(len(speeds)):
         is_above_limit.append(1 if speeds[idx] > speed_limit else 0)
-    return [True, gps.getboundary(gps_coords)] if sum(is_above_limit) > floor((len(speeds) - 1) / 2) else [False, 0]
+    return [True, gps.getboundary(gps_coords)] if sum(is_above_limit) >= floor((len(speeds) - 1) * selection_factor) else [False, 0]
