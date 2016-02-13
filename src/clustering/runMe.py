780f16f8d1a90dd592a207f83852aa6cd746973e
syedshabihhasan <hasanshabih@gmail.com>
2016-02-13 10:31:49 -0600

    removed the the now obselete code

--- src/clustering/runMe.py
+++ src/clustering/runMe.py
@@ -2,7 +2,6 @@ import sys
 import identifyTravel as travel
 import identifyClusters as clusters
 import createClusterPlot as plotcl
-from copy import deepcopy
 import basicDebug as bD
 import preprocessing as pr
 
@@ -49,53 +48,9 @@ def main():
                 travel_clusters.append(gC)
             else:
                 stationary_points += gC
-        #         sc_nz = clusters.getdbscanclusters(gC)
-        #         if sc_nz is not None:
-        #             if not([] == sc_nz['sc']):
-        #                 #stationary_clusters.append(sc_nz['sc'])
-        #                 temp_cls = sc_nz['sc']
-        #                 for cls in temp_cls:
-        #                     stationary_clusters.append(cls)
-        #             if not([] == sc_nz['nz']):
-        #                 noise_markers += sc_nz['nz']
-        # temp_noise_markers = noise_markers
-        # median_cluster_size = clusters.getmedianclustersize(stationary_clusters)
-        # sc_nz = clusters.getdbscanclusters(temp_noise_markers, second_pass= True, median_cluster_size = median_cluster_size)
         print 'collected all points, clustering'
         sc_nz = clusters.getdbscanclusters(stationary_points)
         print 'done'
-        # if sc_nz is not None:
-        #     if not([] == sc_nz['sc']):
-        #         #stationary_clusters.append(sc_nz['sc'])
-        #         temp_cls = sc_nz['sc']
-        #         for cls in temp_cls:
-        #             stationary_clusters.append(cls)
-        #     if not([] == sc_nz['nz']):
-        #         noise_markers = sc_nz['nz']
-        # print 'second pass, original # nz: ' + str(len(temp_noise_markers)) + \
-        #       ' new # nz: ' + str(len(noise_markers)) + \
-        #       ' median cluster size: ' + str(median_cluster_size)
-        # print 'Looking at merging clusters'
-        # stationary_clusters = pr.removesinglevalues(stationary_clusters, less_than=4)
-        # returned_cluster_len = len(stationary_clusters)
-        # last_cluster_len = -1
-        # merged_clusters = deepcopy(stationary_clusters)
-        # print 'merging clusters'
-        # firstpass = True
-        # old_clusters = []
-        # while not (returned_cluster_len == last_cluster_len):
-        #     old_clusters = merged_clusters
-        #     if firstpass:
-        #         firstpass = False
-        #         last_cluster_len = len(merged_clusters)
-        #         merged_clusters = pr.removesinglevalues(merged_clusters, less_than=4)
-        #     else:
-        #         last_cluster_len = returned_cluster_len
-        #     to_merge_with = clusters.intersectingclusters(merged_clusters)
-        #     merged_clusters = clusters.mergeclusters(merged_clusters, to_merge_with)
-        #     returned_cluster_len = len(merged_clusters)
-        #     print 'returned cluster len: ' + str(returned_cluster_len) + ' last cluster len: ' + str(last_cluster_len)
-        # stationary_clusters = merged_clusters
         if sc_nz is not None:
             if not ([] == sc_nz['sc']):
                 stationary_clusters = sc_nz['sc']
